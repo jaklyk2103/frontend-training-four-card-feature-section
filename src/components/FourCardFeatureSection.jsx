8c40860ba4d5b222573bcf3da9774369f2cd7538
Jakub Lyko <jlyko@future-processing.com>
2022-01-08 17:42:08 +0100

    Added texts

--- src/components/FourCardFeatureSection.jsx
+++ src/components/FourCardFeatureSection.jsx
@@ -1,8 +1,19 @@
 import "./FourCardFeatureSection.scss"
+import texts from "../texts"
 
 const FourCardFeatureSection = (props) => {
   return (
     <div className="container">
+      <div className="header1"></div>
+      <div className="header2"></div>
+      <div className="content"></div>
+      <div className="cards-container">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+
     </div>
   )
 }
---
+++ src/texts.js
@@ -0,0 +1,15 @@
+const texts = {
+  'title1': 'Reliable, efficient delivery',
+  'title2': 'Powered by Technology',
+  'content': 'Our Artficial Intelligence powered tools use millions of project data point to ensure that your project is successful',
+  'card1-title': 'Supervisor',
+  'card1-content': 'Monitors activity to identify project roadblocks',
+  'card2-title': 'Team builder',
+  'card2-content': 'Scans our talent network to create the optimal team for your project',
+  'card3-title': 'Karma',
+  'card3-content': 'Regularly evaluates talent to ensure quality',
+  'card4-title': 'Calculator',
+  'card4-content': 'Uses data from past projects to provide better delivery estimates',
+}
+
+export default texts
